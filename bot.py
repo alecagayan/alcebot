f7ac4e1d137cf60f1649487c9d9987b5cca6bb10
oopsie1412 <alecagayan24@gmail.com>
2020-10-10 11:31:57 -0400

    updated help command

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ client = Bot(description=config.des, command_prefix=get_prefix)
 client.load_extension("cogs.prefix")
 client.load_extension("cogs.random")
 client.load_extension("cogs.mod")
-#client.load_extension("cogs.music")
+client.load_extension("cogs.music")
 client.load_extension("cogs.poll")
 client.load_extension("cogs.info")
 
@@ -105,14 +105,6 @@ async def on_ready():
 @client.command()
 async def time(ctx):
     await ctx.send(datetime.datetime.now())
-
-
-#issue
-@client.command()
-async def _joinvc(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
-
 
 #math
 @client.command()
@@ -154,12 +146,6 @@ async def covid(ctx, type, *, state):
         df_county.plot(x='date', y=['cases', 'deaths'], label=['Cases', 'Deaths'], linestyle='-', linewidth=4)
         plt.savefig(county_graph)
         await ctx.send(file=discord.File(county_graph))
-
-
-#shows ms cleckley !
-@client.command()
-async def cleck(ctx):
-    await ctx.send("https://imagen.click/i/7cd655.png")
 
 #sends github link
 @client.command()
@@ -312,28 +298,11 @@ async def netdiskcpu(ctx):
     else:
         await ctx.send(config.err_mesg_permission)
 
-
-#hugs everyone
-@client.command()
-async def hugeveryone(ctx):
-    await ctx.channel.purge(limit=1)
-    await ctx.send("@here has been hugged by " + ctx.message.author.mention + "!")
-    await ctx.send("https://gph.is/g/ajxG084")
-
-
 #marks a suggestion in log
 @client.command()
 async def suggestion(ctx, *, a):
     await ctx.send("Thank you for the suggestion! I will get back to you soon!")
     print ("suggestion: " + a)
-
-
-#BELSON AND TRUMP ARE THE SAME
-@client.command()
-async def belsontrump(ctx):
-    await ctx.send("https://imagen.click/i/b8626b.jpg")
-    await ctx.send("https://imagen.click/i/846a0e.jpg")
-
 
 #Shows the current weather at zip code	
 @client.command()
@@ -387,25 +356,108 @@ async def info(ctx):
 
 client.remove_command('help')
 
-
-#adds help command with embed. embed for big brain
 @client.command()
 async def help(ctx):
+
     embedColor = random.randint(0, 0xffffff)
 
-    embed = discord.Embed(title="alcebot", description="horrible bot = horrible commands. List of commands are:", color=embedColor)
-    embed.add_field(name="Support server", value="[Invite link](https://discord.gg/MJejP9q)")
-    embed.add_field(name="a!info", value="Gives a little info about the bot.", inline=False)
-    embed.add_field(name="a!math <x y z>", value="Gives the operation of **Y** and **Z** using the **X** operation.", inline=False)
-    embed.add_field(name="a!greet", value="Gives a nice greet message.", inline=False)
-    embed.add_field(name="a!roll", value="Roll a random number from 1 to 6.", inline=False)
-    embed.add_field(name="a!weather <zipcode>", value="Gives the latest weather in the area", inline=False)
-    embed.add_field(name="a!compliment <x>", value='"Compliments" the tagged user. If nobody is tagged, prints a random compliment', inline=False)
-    embed.add_field(name="a!help", value="Gives this message. HEEEEEELP!", inline=False)
-    embed.set_footer(text='Requested on ' + str(datetime.datetime.now())) #prints time
-    
-    await ctx.send(embed=embed)
+    message = await ctx.send("Select a page by reacting below!")
+    # getting the message object for editing and reacting
 
+    await message.add_reaction("1️⃣")
+    await message.add_reaction("2️⃣")
+    await message.add_reaction("3️⃣")
+    await message.add_reaction("4️⃣")
+    await message.add_reaction("5️⃣")
+
+    with open('prefixes.json', 'r') as f:
+        prefixes=json.load(f)
+        prefix = prefixes[str(ctx.guild.id)]
+
+    embed1 = discord.Embed(title="Help Page 1/5", description="Need help? Look below", color=embedColor)
+    embed1.add_field(name="Support server", value="[Invite link](https://discord.gg/MJejP9q)")
+    embed1.add_field(name=prefix + "time", value="Reads the time in EST", inline=False)
+    embed1.add_field(name=prefix + "math <x y z>", value="Gives the operation of **Y** and **Z** using the **X** operation.", inline=False)
+    embed1.add_field(name=prefix + "covid <state/county> <name>", value="Gives coronavirus statistics", inline=False)
+    embed1.add_field(name=prefix + "github", value="Gives the link to bot code", inline=False)
+    embed1.add_field(name=prefix + "invite", value="Sends the bot invite", inline=False)
+    embed1.add_field(name=prefix + "roll", value='Rolls a die', inline=False)
+    embed1.add_field(name=prefix + "compliment", value="Compliments a user you tag. If nobody is tagged, a compliment will be printed", inline=False)
+    embed1.set_footer(text='Requested on ' + str(datetime.datetime.now())) #prints time
+
+    embed2 = discord.Embed(title="Help Page 2/5", description="Need help? Look below!", color=embedColor)
+    embed2.add_field(name="Support server", value="[Invite link](https://discord.gg/MJejP9q)")
+    embed2.add_field(name=prefix + "insult", value="Insults a user you tag. If nobody is tagged, an insult will be printed", inline=False)
+    embed2.add_field(name=prefix + "uptime", value="Shows the uptime of the bot", inline=False)
+    embed2.add_field(name=prefix + "xkcd", value="Random XKCD comic", inline=False)
+    embed2.add_field(name=prefix + "server", value="Gives server info", inline=False)
+    embed2.add_field(name=prefix + "ping", value="Pings the bot", inline=False)
+    embed2.add_field(name=prefix + "credit", value='Who made alcebot? Time to find out!', inline=False)
+    embed2.add_field(name=prefix + "hug", value="Hug anyone in the server!", inline=False)
+    embed2.set_footer(text='Requested on ' + str(datetime.datetime.now())) #prints time
+
+    embed3 = discord.Embed(title="Help Page 3/5", description="Need help? Look below", color=embedColor)
+    embed3.add_field(name="Support server", value="[Invite link](https://discord.gg/MJejP9q)")
+    embed3.add_field(name=prefix + "premium", value="You already have premium!", inline=False)
+    embed3.add_field(name=prefix + "suggestion <suggestion>", value="Suggest a new feature or bug fix", inline=False)
+    embed3.add_field(name=prefix + "weather <zip code>", value="Get the weather at your location", inline=False)
+    embed3.add_field(name=prefix + "info", value="Gives basic bot info", inline=False)
+    embed3.add_field(name=prefix + "purge <num of msgs>", value="Purge a certain number of messages", inline=False)
+    embed3.add_field(name=prefix + "netdiskcpu", value='Get info about the bot computer (owner only)', inline=False)
+    embed3.add_field(name=prefix + "fancify <text>", value="Makes text 𝓕𝓐𝓝𝓒𝓨", inline=False)
+    embed3.set_footer(text='Requested on ' + str(datetime.datetime.now())) #prints time
+
+    embed4 = discord.Embed(title="Help Page 4/5", description="Need help? Look below", color=embedColor)
+    embed4.add_field(name="Support server", value="[Invite link](https://discord.gg/MJejP9q)")
+    embed4.add_field(name=prefix + "botplatform", value="Gives a little info about the platform the bot runs on", inline=False)
+    embed4.add_field(name=prefix + "getbans", value="Shows a list of banned users", inline=False)
+    embed4.add_field(name=prefix + "userinfo", value="Gives info on a user", inline=False)
+    embed4.add_field(name=prefix + "christmas", value="Christmas countdown!", inline=False)
+    embed4.add_field(name=prefix + "newyear", value="New Year countdown!", inline=False)
+    embed4.add_field(name=prefix + "ban", value='Bans the tagged user', inline=False)
+    embed4.add_field(name=prefix + "unban", value="Unbans the tagged user", inline=False)
+    embed4.set_footer(text='Requested on ' + str(datetime.datetime.now())) #prints time
+
+    embed5 = discord.Embed(title="Help Page 5/5", description="Need help? Look below", color=embedColor)
+    embed5.add_field(name="Support server", value="[Invite link](https://discord.gg/MJejP9q)")
+    embed5.add_field(name=prefix + "prefix <prefix>", value="Changes the bot prefix", inline=False)
+    embed5.add_field(name=prefix + "enlarge <user>", value="Enlarge a user's profile photo", inline=False)
+    embed5.add_field(name=prefix + "servericon", value="Shows the server's icon", inline=False)
+    embed5.add_field(name=prefix + "mods", value="**In Beta**. Shows the moderators that are online", inline=False)
+    embed5.set_footer(text='Requested on ' + str(datetime.datetime.now())) #prints time
+
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) in ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"]
+
+    while True:
+        try:
+            reaction, user = await client.wait_for("reaction_add", timeout=60, check=check)
+            # waiting for a reaction to be added - times out after x seconds, 60 in this
+            # example
+
+            if str(reaction.emoji) == "1️⃣":
+                await message.edit(embed=embed1)
+                await message.remove_reaction(reaction, user)
+            elif str(reaction.emoji) == "2️⃣":
+                await message.edit(embed=embed2)
+                await message.remove_reaction(reaction, user)
+            elif str(reaction.emoji) == "3️⃣":
+                await message.edit(embed=embed3)
+                await message.remove_reaction(reaction, user)
+            elif str(reaction.emoji) == "4️⃣":
+                await message.edit(embed=embed4)
+                await message.remove_reaction(reaction, user)
+            elif str(reaction.emoji) == "5️⃣":
+                await message.edit(embed=embed5)
+                await message.remove_reaction(reaction, user)
+            else:
+                await message.remove_reaction(reaction, user)
+                # removes reactions if the user tries to go forward on the last page or
+                # backwards on the first page
+        except asyncio.TimeoutError:
+            await message.delete()
+            break
+            # ending the loop if user doesn't react after x seconds
 
 #purge cmd	
 @client.command()
@@ -447,33 +499,6 @@ async def hug(ctx, *, member: discord.Member = None):
     except Exception as e:
         print('alce is a fuckup, here\'s his shitty error:' + e)
     return
-
-
-#bot replies
-@client.command(aliases=['say'])
-async def echo(ctx, *msg):
-    """Makes the bot talk."""
-    try:
-        say = ' '.join(msg)
-        await client.delete_message(ctx.message)
-        return await ctx.send(say)
-    except Exception as e:
-        print('alce is a fuckup, here\'s his shitty error:' + e)
-    return
-
-
-#bot replies with tts message
-@client.command(aliases=['saytts'])
-async def echotts(ctx, *msg):
-    """Makes the bot talk, with TTS."""
-    try:
-        say = ' '.join(msg)
-        await client.delete_message(ctx.message)
-        return await ctx.send(say, tts=True)
-    except Exception as e:
-        print('alce is a fuckup, here\'s his shitty error:' + e)
-    return
-
 
 @client.command(aliases=["fancy"])
 async def fancify(ctx, *, text):
