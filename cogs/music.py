0794e620121a58dfd1579fa1be210a423c5bbee7
gido5731 <gido@clickable.systems>
2021-01-08 19:38:09 -0500

    Allow for ipv6 because if it fails "Because of v6" then the problem isnt the devs, it's you

--- cogs/music.py
+++ cogs/music.py
@@ -20,7 +20,6 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
 ffmpeg_options = {
